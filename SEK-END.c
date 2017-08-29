69c1494c6a18ce4261eb95d7115b0bf1bf8dcf48
UmBeloGramadoVerde <kaynarodrigues@live.com>
2017-08-29 10:29:05 -0300

    Add files via upload

---
+++ SEK-END.c
@@ -0,0 +1,22 @@
+#define MOTOR_ESQUERDA OUT_A
+#define MOTOR_DIREITA OUT_C
+#define SENSOR_ESQUERDA IN_1
+#define SENSOR_DIREITA IN_2
+#define SENSOR_US_ESQUERDA IN_3
+#define SENSOR_US_DIREITA IN_4
+#define VELOCIDADE_BAIXA 35
+#define VELOCIDADE_ALTA 65
+#define PRETO 1
+#define VERDE 3
+#define BRANCO 6
+#define AZUL 2
+#define VERMELHO 5
+#define AMARELO 4
+
+
+task main ()
+{
+
+
+
+}
